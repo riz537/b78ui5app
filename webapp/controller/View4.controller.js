7adb9ca0bf0453bc3a04ea3f8af5b059ed236e2e
Rizwan Shaik <rizwantrainingsb78p1@gmail.com>
2026-02-13 04:59:48 +0000

    create deep update

--- webapp/controller/View4.controller.js
+++ webapp/controller/View4.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox"
-], (Controller,MessageBox) => {
+], (Controller, MessageBox) => {
     "use strict";
 
     return Controller.extend("com.demo.b78sapui5.controller.View4", {
@@ -11,15 +11,39 @@ sap.ui.define([
         onPatternMatched: function (oEvent) {
             var empId = oEvent.getParameter("arguments").key;
             this.getView().bindElement("oModel>/EmployeeSet('" + empId + "')");
+
+            // read certification data
+            var oModel = this.getOwnerComponent().getModel("oModel");
+            oModel.read("/EmployeeSet('" + empId + "')/toCertifications", {
+                success: function (data, response) {
+                    this.getOwnerComponent().getModel("certUpdateModel").setData(data);
+                }.bind(this)
+            });
+        },
+        onPressAdd: function () {
+           this.getOwnerComponent().getModel("certUpdateModel").getData().results.push(
+                {
+                    Empid: this.byId("oIpEmpId").getValue(),
+                    Certcode: "",
+                    Skill: "",
+                    Certname: ""
+                }
+            );
+            this.getOwnerComponent().getModel("certUpdateModel").refresh();
+        },
+        onDeleteRow: function (oEvent) {
+            /// you need to find out the index and use splice function on the array
+            var index = oEvent.getSource().getParent().getBindingContextPath().split("/")[2];
+            this.getOwnerComponent().getModel("certUpdateModel").getData().results.splice(index, 1);
+            this.getOwnerComponent().getModel("certUpdateModel").refresh();
         },
         onNavBack: function () {
             this.getOwnerComponent().getRouter().navTo("RouteView1");
         },
-        onPresSave:function(){
-             var empId = this.byId("oIpEmpId").getValue();
+        onPresSave: function () {
+            var empId = this.byId("oIpEmpId").getValue();
             var name = this.byId("oIpName").getValue();
             var desig = this.byId("oIpDesig").getValue();
-            var skill = this.byId("oIpSkill").getValue();
             var email = this.byId("oIpEmail").getValue();
             var salary = this.byId("oIpSalary").getValue();
             var status = this.byId("oIpStatus").getValue();
@@ -31,19 +55,19 @@ sap.ui.define([
                 Empid: empId,
                 Name: name,
                 Desig: desig,
-                Skill: skill,
                 Email: email,
                 Salary: salary,
                 Status: status,
                 Rating: parseInt(rating),
+                toCertifications: this.getOwnerComponent().getModel("certUpdateModel").getData().results
             };
             var oModel = this.getOwnerComponent().getModel("oModel");
-            oModel.update("/EmployeeSet('"+empId+"')",payload,{
-                success:function(req,res){
-                     MessageBox.success("Employee Updated Successfully");
+            oModel.create("/EmployeeSet", payload, {
+                success: function (req, res) {
+                    MessageBox.success("Employee Updated Successfully");
                 },
-                error:function(oError){
-                  MessageBox.error(JSON.parse(oError.responseText).error.message.value);
+                error: function (oError) {
+                    MessageBox.error(JSON.parse(oError.responseText).error.message.value);
                 }
             });
         }
